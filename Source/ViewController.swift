f3f3ad80f2a9c84cde11422326730c4bf319507f
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-06-21 09:20:33 +0900

    ArisiaCard: initial commit

---
+++ Source/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ArisiaCard
+//
+//  Created by Tomoo Hamada on 2025/06/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+
+                // Do any additional setup after loading the view.
+        }
+
+        override var representedObject: Any? {
+                didSet {
+                // Update the view, if already loaded.
+                }
+        }
+
+
+}
+
