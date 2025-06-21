a6775e1eeba25b7cd330fd2c07b03d088e13ed41
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-06-21 09:30:36 +0900

    project: add application for iOS

---
+++ Project/ArisiaCard_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArisiaCard_iOS
+//
+//  Created by Tomoo Hamada on 2025/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+        }
+
+
+}
+
