9b7105e45972c8a6c9c6c5b66fc1e73b81719ed2
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-07-20 07:23:29 +0900

    StackViewController: Udate to support drag and drop

--- Source/StackViewController.swift
+++ Source/StackViewController.swift
@@ -6,25 +6,66 @@
  */
 
 import MultiUIKit
+import MultiFrameKit
+import JavaScriptCore
 import Foundation
 
 public class StackViewController: MIViewController
 {
+        private var mContext:           MFContext?     = nil
+        private var mConsoleStorage:    MITextStorage? = nil
+
         public override func viewDidLoad() {
                 super.viewDidLoad()
+
+                let vm   = JSVirtualMachine()
+                let ctxt = MFContext(virtualMachine: vm)
+                mContext = ctxt
 
                 let root = MIStack()
                 root.axis = .vertical
 
-                let buttonimg = MIIconView()
-                buttonimg.set(symbol: .buttonHorizontalTopPress, size: .regular)
-                root.addArrangedSubView(buttonimg)
+                /*
+                 * Add views for stack
+                 */
+                let stack = MFDropView()
+                stack.contentsView.axis = .vertical
+                root.addArrangedSubView(stack)
 
                 let button = MIButton()
                 button.title = "Hello"
-                root.addArrangedSubView(button)
+                stack.contentsView.addArrangedSubView(button)
+
+                /*
+                 * Add views for development
+                 */
+                let devbox = MIStack()
+                devbox.axis = .horizontal
+                root.addArrangedSubView(devbox)
+
+                let buttonimg = MIIconView()
+                buttonimg.set(symbol: .buttonHorizontalTopPress, size: .regular)
+                devbox.addArrangedSubView(buttonimg)
+
+                let console = MITextView()
+                console.isEditable = false
+                devbox.addArrangedSubView(console)
+                mConsoleStorage = console.textStorage
 
                 self.view = root
                 //root.setFrameSize(NSSize(width: 320, height: 240))
+
+                /* setup JavaScript context */
+                boot(context: ctxt)
+        }
+
+        private func boot(context ctxt: MFContext) {
+                guard let storage = mConsoleStorage else {
+                        NSLog("[Error] Parameter is NOT set")
+                        return
+                }
+
+                /* set "console" */
+                MFConsole.boot(storage: storage, context: ctxt)
         }
 }
